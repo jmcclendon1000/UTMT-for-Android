74d59db854e9605fcba52c63a28f3b18489dde11
Genouka <142009020+Genouka@users.noreply.github.com>
2026-01-15 23:31:19 +0800

    fix: cannot correctly localization on Android

--- tool/action_build.bat
+++ tool/action_build.bat
@@ -10,10 +10,12 @@ set "classesfile=%ExecutePath%classes.dex"
 
 :: Prebuild Resources only for github actions because local build will automatically prebuild resources
 call "%ExecutePath%prebuild_resources.bat"
-
+msbuild "%ExecutePath%..\UndertaleModToolAvalonia\UndertaleModToolAvalonia.csproj" /p:Configuration=Debug /p:Platform="Any CPU"
 msbuild "%ExecutePath%..\UndertaleModToolAvalonia.Android\UndertaleModToolAvalonia.Android.csproj" /t:SignAndroidPackage /p:Configuration=Debug /p:Platform="Any CPU"
 rmdir /s /q "%ExecutePath%..\UndertaleModToolAvalonia.Android\obj\Any CPU\Debug\net9.0-android\android\.__override__"
 ren "%ExecutePath%..\UndertaleModToolAvalonia.Android\obj\Any CPU\Debug\net9.0-android\android\assets" ".__override__"
+xcopy "%ExecutePath%..\UndertaleModToolAvalonia\obj\Any CPU\Debug\net9.0" "%ExecutePath%..\UndertaleModToolAvalonia.Android\obj\Any CPU\Debug\net9.0-android\android\.__override__\arm64-v8a" /s /e /y
+rmdir /s /q "%ExecutePath%..\UndertaleModToolAvalonia.Android\obj\Any CPU\Debug\net9.0-android\android\.__override__\x86_64"
 mkdir "%ExecutePath%assets" 2>nul
 "%ExecutePath%7z.exe" a -tzip "%ExecutePath%assets\genouka_patcher.ext" "%ExecutePath%..\UndertaleModToolAvalonia.Android\obj\Any CPU\Debug\net9.0-android\android\.__override__"
 "%ExecutePath%7z.exe" a -tzip "%file%" "%ExecutePath%assets" -aoa
